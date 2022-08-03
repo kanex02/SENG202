3c5088dd935ca3786d48797d0a57bbe49d04e3e5
kfi39 <kfi39@canterbury.ac.nz>
2022-08-03 12:03:23 +1200

    KF Tet push

--- src/main/java/seng202/team4/App.java
+++ src/main/java/seng202/team4/App.java
@@ -20,6 +20,7 @@ public class App {
     public static void main(String[] args) {
         //SINGLE LINE COMMENT!!
         System.out.println("Test");
+        System.out.println("Testing");
         log.info("Hello World!");
         log.warn("This is a warning message! Use this log type to 'warn' if something is not quite right");
         log.error("An error has occurred, thanks logging for helping find it! (This is a terrible error log message, but is only an example!')");
