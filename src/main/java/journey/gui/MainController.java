ad3f18a92f35c47071f7bff710d50e368f540ac2
Tom Barthelmeh <tba105@uclive.ac.nz>
2022-08-16 00:09:59 +1200

    Initial GUI prototype. Changed main to main.fxml. Updated revolt to journey in pom.xml

--- src/main/java/journey/gui/MainController.java
+++ src/main/java/journey/gui/MainController.java
@@ -1,48 +1,51 @@
-//package journey.gui;
-//
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.Label;
-//import javafx.stage.Stage;
-//import journey.controller.CounterService;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//
-///**
-// * Controller for the main window
-// * @author seng202 teaching team
-// */
-//public class MainController {
-//
-//    private static final Logger log = LogManager.getLogger();
-//
-//    @FXML
-//    private Label defaultLabel;
-//
-//    @FXML
-//    private Button defaultButton;
-//
-//    private CounterService counterService;
-//
-//    /**
-//     * Initialize the window
-//     *
-//     * @param stage Top level container for this window
-//     */
+package journey.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.Event;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Controller for the main window
+ * @author seng202 teaching team
+ */
+public class MainController {
+
+    private static final Logger log = LogManager.getLogger();
+
+    @FXML private ChoiceBox chargerBox;
+    @FXML private TextField registrationTextBox;
+    @FXML private Button viewPrevJourneys;
+    @FXML private ComboBox filterList;
+    @FXML private ComboBox sortList;
+    @FXML private AnchorPane scrollPane_inner;
+    @FXML private Button recordJourneyStartButton;
+    @FXML private Button recordJourneyEndButton;
+    @FXML private TextField chargingStationTextField;
+    @FXML private TextArea stationDetailTextArea;
+
+    @FXML private void userDropdown(Event event) {
+        log.info("User dropdown button pressed!");
+    }
+
+
+    /**
+     * Initialize the window
+     *
+     * @param stage Top level container for this window
+     */
 //    public void init(Stage stage) {
-//        counterService = new CounterService();
 //    }
-//
-//    /**
-//     * Method to call when our counter button is clicked
-//     *
-//     */
-//    @FXML
-//    public void onButtonClicked() {
-//        log.info("Button has been clicked");
-//        counterService.incrementCounter();
-//
-//        int count = counterService.getCurrentCount();
-//        defaultLabel.setText(Integer.toString(count));
-//    }
-//}
+
+    /**
+     * Method to call when our counter button is clicked
+     *
+     */
+    @FXML
+    public void onButtonClicked() {
+        log.info("Button has been clicked");
+    }
+}
