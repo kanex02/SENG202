e1b90affa043f0b1aceab5486d6e1749f861d487
Tom Barthelmeh <tba105@uclive.ac.nz>
2022-08-24 11:34:35 +1200

    Uncommented main files.

--- src/main/java/journey/App.java
+++ src/main/java/journey/App.java
@@ -1,35 +1,35 @@
-//package journey;
-//
-//import journey.gui.MainWindow;
-//import org.apache.logging.log4j.Level;
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//
-///**
-// * Default entry point class.
-//
-// * @author seng202 teaching team.
-// */
-//public class App {
-//    private static final Logger log = LogManager.getLogger();
-//
-//    /**
-//     * Entry point which runs the javaFX application.
-//     * Also shows off some different logging levels.
-//
-//     * @param args program arguments from command line.
-//     */
-//    public static void main(String[] args) {
-//        //SINGLE LINE COMMENT!!
-//        //SECOND SINGLE LINE COMMENT!!!
-//        System.out.println("Test");
-//        System.out.println("Testing");
-//        log.info("Hello World!");
-//        log.warn("This is a warning message! Use this log type to 'warn' if something is not quite right");
-//        log.error("An error has occurred, thanks logging for helping find it! (This is a terrible error log message, "
-//                + "but is only an example!')");
-//        log.log(Level.INFO, "There are many ways to log!");
-//
-//        MainWindow.main(args);
-//    }
-//}
+package journey;
+
+import journey.gui.MainWindow;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Default entry point class.
+
+ * @author seng202 teaching team.
+ */
+public class App {
+    private static final Logger log = LogManager.getLogger();
+
+    /**
+     * Entry point which runs the javaFX application.
+     * Also shows off some different logging levels.
+
+     * @param args program arguments from command line.
+     */
+    public static void main(String[] args) {
+        //SINGLE LINE COMMENT!!
+        //SECOND SINGLE LINE COMMENT!!!
+        System.out.println("Test");
+        System.out.println("Testing");
+        log.info("Hello World!");
+        log.warn("This is a warning message! Use this log type to 'warn' if something is not quite right");
+        log.error("An error has occurred, thanks logging for helping find it! (This is a terrible error log message, "
+                + "but is only an example!')");
+        log.log(Level.INFO, "There are many ways to log!");
+
+        MainWindow.main(args);
+    }
+}
