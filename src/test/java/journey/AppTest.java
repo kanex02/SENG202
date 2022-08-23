c34cb39ecfdecfcc6e6e50c35fede0d6b82077b7
Tom Barthelmeh <tba105@uclive.ac.nz>
2022-08-24 11:44:36 +1200

    added failing test

--- src/test/java/journey/AppTest.java
+++ src/test/java/journey/AppTest.java
@@ -18,33 +18,7 @@ public class AppTest
         assertTrue( true );
     }
 
-    /**
-     * KX test
-     */
     @Test
-    public void testKX(){
-        assertFalse(false);
-    }
-
-    @Test
-    public void tomTest()
-    {
-        assertEquals(1+1,2);
-    }
-
-    @Test
-    public void danielTest()
-    {
-        assertEquals(9+10,19);
-    }
-
-    @Test
-    public void ellaTest()
-    {
-        assertEquals(1+4,5);
-    }
-
-    @Test
-    public void katieTest() { assertEquals(1+3,4); }
+    public void failingTest() { fail(); }
 
 }
