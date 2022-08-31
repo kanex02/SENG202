888b1beae83e9be4e6b208ff0f0e1cb6ad35a27e
Kane Xie <kxi12@uclive.ac.nz>
2022-08-31 23:30:22 +1200

    Added station viewer window

--- src/main/java/journey/controller/MainController.java
+++ src/main/java/journey/controller/MainController.java
@@ -3,18 +3,23 @@ package journey.controller;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.event.Event;
 import javafx.scene.layout.AnchorPane;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import journey.data.Database;
 import journey.data.QueryResult;
 import journey.data.Station;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -93,6 +98,30 @@ public class MainController {
         System.out.println("New vehicle with registration " + registration
         + " and charger type " + chargerTypeChoice);
         event.consume();
+    }
+
+    // Opens up the station view.
+    @FXML private void viewStations(Event event) {
+        Parent root;
+        try {
+            FXMLLoader baseLoader = new FXMLLoader(getClass().getResource("/fxml/table.fxml"));
+            root = baseLoader.load();
+
+            TableService controller = baseLoader.getController();
+
+            Stage tableStage = new Stage(StageStyle.UNDECORATED);
+            controller.getData(tableStage);
+
+            tableStage.setTitle("Stations");
+            Scene scene = new Scene(root);
+            tableStage.setScene(scene);
+            tableStage.show();
+            tableStage.setMinHeight(500);
+            tableStage.setMinWidth(800);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     // Populates the station list with expandable buttons.
