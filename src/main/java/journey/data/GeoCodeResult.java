94621ac85dc40599ab6db851e8942b1112942315
Daniel <dne33@uclive.ac.nz>
2022-09-28 23:20:25 +1300

    Added classes that add geolocation and geocoding functionality to app. These have not been integrated but work when manually testing.

---
+++ src/main/java/journey/data/GeoCodeResult.java
@@ -0,0 +1,21 @@
+package journey.data;
+
+/**
+ * Simple class wrapping string of address for use with Geocoding.
+ * @author Daniel Neal
+ */
+public class GeoCodeResult {
+    String address;
+
+    /**
+     * Creates a new geocode result with the given address.
+     * @param address address of location.
+     */
+    public GeoCodeResult(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
