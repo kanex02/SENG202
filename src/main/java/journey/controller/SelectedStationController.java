6e37ebdbc959631dfa85c9b3e1ca4510aa11b080
eca66 <eca66@uclive.ac.nz>
2022-10-15 15:36:32 +1300

    adding list of connectors to information about station

--- src/main/java/journey/controller/SelectedStationController.java
+++ src/main/java/journey/controller/SelectedStationController.java
@@ -1,11 +1,22 @@
 package journey.controller;
 
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import journey.data.Note;
 import journey.data.Station;
 import journey.repository.NoteDAO;
 import journey.repository.StationDAO;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Controller for selected station FXML.
@@ -17,15 +28,14 @@ public class SelectedStationController {
     @FXML private Label nameField;
     @FXML private Label operatorField;
     @FXML private Label currentField;
-    @FXML private Label connectorField;
     @FXML private Label carparkField;
     @FXML private Label timeLimitField;
     @FXML private Label costField;
     @FXML private Label parkingCostField;
     @FXML private Label ratingField;
     @FXML private Label favouritedField;
-    @FXML private Label attractionsField;
     @FXML private Label operationalWarning;
+    @FXML private ListView<String> connectorsList;
 
     private final StationDAO stationDAO = new StationDAO();
     private final NoteDAO noteDAO = new NoteDAO();
@@ -41,7 +51,6 @@ public class SelectedStationController {
         nameField.setText(selectedStation.getName());
         operatorField.setText(selectedStation.getOperator());
         currentField.setText(selectedStation.getCurrentType());
-        connectorField.setText(Integer.toString(selectedStation.getNumberOfConnectors()));
         carparkField.setText(Integer.toString(selectedStation.getCarParkCount()));
         int time = selectedStation.getMaxTime();
         if (time == 0) {
@@ -55,19 +64,38 @@ public class SelectedStationController {
         parkingCostField.setText(parkCost ? "Yes" : "No");
         Note notes = noteDAO.getNoteFromStation(selectedStation, mainController.getCurrentUser());
         favouritedField.setText(notes.getFavourite() ? "Yes" : "No");
-        boolean attractions = selectedStation.getHasTouristAttraction();
-        attractionsField.setText(attractions ? "Yes" : "No");
         if (notes.getRating() != 0) {
             ratingField.setText(Integer.toString(notes.getRating()));
         } else {
             ratingField.setText("Not yet rated");
         }
-        String operational = selectedStation.getDateFirstOperational();
-        if (operational.length() < 1) {
-            operationalWarning.setText("WARNING: This station is not yet operational!");
+        String[] connectors = selectedStation.getConnectors();
+        boolean inoperative = false;
+        ObservableList<String> conns = FXCollections.observableArrayList();
+        int counter = 1;
+        for (String connector: connectors) {
+            if (!inoperative) {
+                inoperative = connector.contains("Unknown") || connector.contains("Inoperative");
+            }
+            if (counter % 2 == 0) {
+                String conn = null;
+                if (counter == 2) {
+                    conn = (connectors[counter-2] + ":" + connectors[counter-1] + ":" + connectors[counter].substring(0, 1));
+                } else {
+                    conn = (connectors[counter-2].substring(3) + ":" + connectors[counter-1] + ":" + connectors[counter].substring(0, 1));
+                }
+                conn.substring(0, conn.length()-2);
+                conns.add(conn);
+            }
+            counter++;
+        }
+        if (inoperative) {
+            operationalWarning.setText("WARNING: This station may have inoperative chargers!");
         } else {
             operationalWarning.setText("");
         }
+        connectorsList.getItems().addAll(conns);
+
     }
 
     /**
